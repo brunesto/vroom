e21e605b6ca0a83ed7b9e17fdce369bbd11146dd
brunesto <bruno.carle@gmail.com>
2025-12-27 12:18:00 +0100

    log

--- src/utils/log.h
+++ src/utils/log.h
@@ -1,7 +1,15 @@
 #ifndef LOG_H
 #define LOG_H
+#include <cstring>
 #include <iostream>
-#define TRACE_LOG(x) std::cerr << "👾 [TRACE] " << __FILE__ << ":" << __LINE__ << " -> " << x << std::endl
-#define DEBUG_LOG(x) std::cerr << "🪲 [DEBUG] " << __FILE__ << ":" << __LINE__ << " -> " << x << std::endl
-#define INFO_LOG(x) std::cerr << " ℹ️ [INFO] " << __FILE__ << ":" << __LINE__ << " -> " << x << std::endl
+
+
+inline const char* get_basename(const char* path) {
+  const char* file = strrchr(path, '/');
+  return file ? file + 1 : path;
+}
+
+#define TRACE_LOG(x) std::cerr << "👾 [TRACE] " << get_basename(__FILE__) << ":" << __LINE__ << " -> " << x << std::endl
+#define DEBUG_LOG(x) std::cerr << "🪲 [DEBUG] " << get_basename(__FILE__) << ":" << __LINE__ << " -> " << x << std::endl
+#define INFO_LOG(x) std::cerr << " ℹ️ [INFO] " << get_basename(__FILE__) << ":" << __LINE__ << " -> " << x << std::endl
 #endif
